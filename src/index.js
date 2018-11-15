2ca823294d6cc0e27ba0cd46f453dca410d9e405
Joshua Elder <jcelder88@gmail.com>
2018-11-14 21:14:29 -0800

    Add timeout for requests

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ const path = require('path');
 const Promise = require('bluebird');
 const fs = Promise.promisifyAll(require('fs'))
 const request = require('request');
-const rp = require('request-promise');
 const sprintf = require('sprintf-js').sprintf;
 
 const imageDir = 'images';
-const runs = 40;
+const runs = 5;
 
 const urlOptions = {
   baseUrl: 'https://loremflickr.com', 
@@ -14,19 +13,24 @@ const urlOptions = {
   height: '853',
   topic: 'nature'
 };
+const timeoutInterval = 2000;
+let timeout = 0;
 
 const url = `${urlOptions.baseUrl}/${urlOptions.width}/${urlOptions.height}/${urlOptions.topic}`
 
 
 for (let i = 1; i <= runs; i++) {
-    let imageName = sprintf('%05s.jpg', i)
+    let imageName = sprintf('%05s.jpg', i) // image format style is 00001.jpg
     let imagePath = path.join(imageDir, imageName)
-    let stream = request(url).pipe(fs.createWriteStream(imagePath))
-    stream.on('finish', (err) => {
-      if (err) {
-        console.error(err)
-      } else {
-        console.log(`Image ${i} written to images/${imageName}`)
-      }
-    })
+    setTimeout(() => {
+      let stream = request(url).pipe(fs.createWriteStream(imagePath))
+      stream.on('finish', (err) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(`Image ${i} written to images/${imageName}`);
+        }
+      });
+    }, timeout);
+    timeout += timeoutInterval
 };
